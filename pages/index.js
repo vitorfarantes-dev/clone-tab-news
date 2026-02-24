b3df8ff5427560e753a5abbc912ed0dc408e8b02
vitorfarantes-dev <vitor.f.arantes@gmail.com>
2026-02-24 11:30:12 +0000

    Change index.js message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Valeu Luan, por me disponiblizar a oportunidade de estudar no curso.dev</h1>
+    return <h1>Oi Amor! Meu site está na internet e eu te amo!</h1>
 }
 
 export default Home;
